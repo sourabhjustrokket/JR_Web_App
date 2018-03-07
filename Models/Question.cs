b73c906f3399a6cb45b6f398fd5e060521ac96ec
Sourabh Somani <sourabh_somani2010@hotmail.com>
2018-03-07 22:42:20 +0530

    Bootswatch added and Create Question Page Created
    Bootswatch added and Create Question Page Created

---
+++ Models/Question.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JR_Web_App.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Detail { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public List<Tag> tags { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
